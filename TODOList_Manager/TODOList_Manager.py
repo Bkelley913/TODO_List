b5377f3577d2c7b403830cd6591586f40eadd1e0
Blick913 <94084594+Blick1013@users.noreply.github.com>
2023-07-12 16:08:59 -0400

    Testing the database and adding random tasks to make sure everything is displaying and working

--- TODOList_Manager/TODOList_Manager.py
+++ TODOList_Manager/TODOList_Manager.py
@@ -17,6 +17,9 @@ create_table_query = '''
 '''
 cursor.execute(create_table_query)
 
+select_query = "SELECT * FROM tasks"
+cursor.execute(select_query)
+
 # Insert a task into the table
 insert_task_query = "INSERT INTO tasks (task) VALUES ('Complete project')"
 cursor.execute(insert_task_query)
@@ -31,3 +34,48 @@ conn.commit()
 
 # Close the database connection
 conn.close()
+
+def display_task():
+    # Establish a connection to the SQLite database
+    conn = sqlite3.connect('todo.db')
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Select all rows from the tasks table
+    select_query = "SELECT * FROM tasks"
+    cursor.execute(select_query)
+    
+    # Fetch all the rows as a list of tuples
+    rows = cursor.fetchall()
+
+    # Print the data
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    conn.close()
+
+# Call the function to display the tasks
+display_task()
+
+def add_task(*args):
+    # Establish a connection to the SQLite database
+    conn = sqlite3.connect('todo.db')
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Execute the INSERT statement
+    for task in args:
+        insert_query = "INSERT INTO tasks (task) VALUES (TEXT)"
+        cursor.execute(insert_query, (task,))
+
+    # Commit the changes to the database
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
+
+# Call the function to add multiple tasks
+add_task("Complete project", "Review documentation", "Submit report")
